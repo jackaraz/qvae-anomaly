6610a49973336f708676a7e3e411c59beb34299d
Jack Y. Araz <jackaraz@gmail.com>
2024-08-05 15:37:25 -0400

    bugfix

--- qVAE.py
+++ qVAE.py
@@ -267,7 +267,7 @@ def get_cost(circuit, optimizer, linear_loss: bool = False, parallelise: bool = 
         pars = optax.apply_updates(pars, updates)
         return loss, pars, opt_state
 
-    return batch_cost, jax.jit(train_step) if parallelise else train_step
+    return batch_cost, jax.jit(train_step) if not parallelise else train_step
 
 
 def circle(
